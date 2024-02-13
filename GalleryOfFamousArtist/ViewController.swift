326ec6c518dd9f6046d545360a11bf3b107acad4
Flyquiz <Martiannickel@icloud.com>
2024-02-13 14:57:54 +0300

    Initial Commit

---
+++ GalleryOfFamousArtist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryOfFamousArtist
+//
+//  Created by Иван Захаров on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
